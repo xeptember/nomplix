3c6b61193ba54c2d3ee52c9aaec9b010c579641d
hmpark <hmpark@creativehill.co.kr>
2021-03-17 14:52:59 +0900

    router

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 import Router from "Components/Router";
+import Header from "Components/Header";
 function App() {
   return (
     <>
+      <Header />
       <Router />
     </>
   );
--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import Home from "Routes/Home";
 import Search from "Routes/Search";
 import TV from "Routes/TV";
@@ -10,6 +10,7 @@ export default () => {
             <Route path="/" exact component={Home} />
             <Route path="/tv" exact component={TV} />
             <Route path="/search" exact component={Search} />
+            <Redirect from="*" to="/" />
         </Router>
     )
 }
