b42bf6c1a07f64e543aa32a09e1e42adb28f9cde
hmpark <hmpark@creativehill.co.kr>
2021-03-17 15:00:42 +0900

    Switch

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Redirect,   } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect, Switch  } from "react-router-dom";
 import Home from "Routes/Home";
 import Search from "Routes/Search";
 import TV from "Routes/TV";
@@ -7,10 +7,12 @@ import TV from "Routes/TV";
 export default () => {
     return (
         <Router>
-            <Route path="/" exact component={Home} />
-            <Route path="/tv" exact component={TV} />
-            <Route path="/search" exact component={Search} />
-            <Redirect from="*" to="/" />
+            <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/tv" exact component={TV} />
+                <Route path="/search" exact component={Search} />
+                <Redirect from="*" to="/" />
+            </Switch>
         </Router>
     )
 }
