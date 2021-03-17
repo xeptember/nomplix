b3658a0d0e5545ac58325add4daf9f66c767e62d
hmpark <hmpark@creativehill.co.kr>
2021-03-17 13:41:05 +0900

    first commit

---
+++ src/Components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import Router from "Components/Router";
+function App() {
+  return (
+    <>
+      <Router />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Router.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "Routes/Home";
+import Search from "Routes/Search";
+import TV from "Routes/TV";
+
+export default () => {
+    return (
+        <Router>
+            <Route path="/" exact component={Home} />
+            <Route path="/tv" exact component={TV} />
+            <Route path="/search" exact component={Search} />
+        </Router>
+    )
+}
