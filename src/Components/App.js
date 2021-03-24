a75ad7ac4b658924fef2bea3fbd4f9b89663398e
hmpark <hmpark@creativehill.co.kr>
2021-03-24 15:04:56 +0900

    styled components

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,7 +4,6 @@ import Header from "Components/Header";
 function App() {
   return (
     <>
-      <Header />
       <Router />
     </>
   );
--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Redirect, Switch  } from "react-router-dom";
+import Header from "Components/Header";
 import Home from "Routes/Home";
 import Search from "Routes/Search";
 import TV from "Routes/TV";
@@ -7,12 +8,15 @@ import TV from "Routes/TV";
 export default () => {
     return (
         <Router>
+            <>
+            <Header />
             <Switch>
                 <Route path="/" exact component={Home} />
                 <Route path="/tv" exact component={TV} />
                 <Route path="/search" exact component={Search} />
                 <Redirect from="*" to="/" />
             </Switch>
+            </>
         </Router>
     )
 }
