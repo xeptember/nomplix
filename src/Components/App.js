b29e4c8a65c2a0f53e5c01d35e4cca737b8421c3
hmpark <hmpark@creativehill.co.kr>
2021-03-24 15:54:45 +0900

    styled Header

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import Router from "Components/Router";
 import Header from "Components/Header";
+import GlobalStyles from "Components/GlobalStyles";
 function App() {
   return (
     <>
       <Router />
+      <GlobalStyles />
     </>
   );
 }
