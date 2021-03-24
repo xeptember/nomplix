4daf41333784f9440caeb6577349d8b4de0d277e
hmpark <hmpark@creativehill.co.kr>
2021-03-24 17:44:46 +0900

    api.js

--- src/api.js
+++ src/api.js
@@ -11,10 +11,30 @@ const api = axios.create({
 export const moviesApi = {
     nowPlaying: () => api.get("movie/now_playing"),
     upcoming: () => api.get("movie/upcoming"),
-    popular: () => api.get("movie/popular")
+    popular: () => api.get("movie/popular"),
+    movieDetail: id => api.get(`movie/${id}`, {
+        params: {
+            append_to_response: "videos"
+        }
+    }),
+    search: term => api.get("search/movie", {
+        params: {
+            query: term
+        }
+    })
 }
 export const TVApi = {
     topRated: () => api.get("tv/top_rated"),
     popular: () => api.get("tv/popular"),
-    airingToday: () => api.get("tv/airing_today")
+    airingToday: () => api.get("tv/airing_today"),
+    showDetail: id => api.get(`tv/${id}`, {
+        params: {
+            append_to_response: "videos"
+        }
+    }),
+    search: term => api.get("search/tv", {
+        params: {
+            query: term
+        }
+    })
 }
